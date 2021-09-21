0a5c2a16a294f5ca2791e28f3c093b2e49f17f09
hiroki917 <kensmith_5276_zemaitis@yahoo.co.jp>
2021-09-21 20:03:36 +0900

    テストコード

--- app/models/music.rb
+++ app/models/music.rb
@@ -1,13 +1,15 @@
 class Music < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :genre
+  belongs_to :user
   has_one_attached :image
-  
+
   with_options presence: true do 
     validates :name
     validates :phrase
     validates :text
     validates :genre_id
+    validates :image
   end
 
   with_options numericality: { other_than: 1, message: "can't be blank" } do
